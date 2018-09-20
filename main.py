0b0b526db60b1faa2d44e7762bd72499c95a0260
Poloz Alexey <polozhev@mail.ru>
2018-09-20 14:02:58 +0300

    Exception timeout

--- main.py
+++ main.py
@@ -297,4 +297,9 @@ def handle_message(message):
 		
 
 if __name__ == '__main__':
-	bot.polling(none_stop=True)
+	while True:
+		try:
+			bot.polling(none_stop=True)
+		except Exception as e:
+			print(e)
+			time.sleep(5)
